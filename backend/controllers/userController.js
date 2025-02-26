19e1751cddcee629d2f8b113d7837767b9e54021
pery <oluwapery99@gmail.com>
2025-02-26 14:01:46 +0100

    changes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,10 @@
-const User = require('../models/User');
+const User = require("../models/User");
 
 exports.getProfile = async (req, res) => {
   try {
     const user = await User.findById(req.params.userId)
-      .select('-password')
-      .populate('friends', 'username profilePicture');
+      .select("-password")
+      .populate("friends", "username profilePicture");
     res.json(user);
   } catch (error) {
     res.status(400).json({ error: error.message });
@@ -14,19 +14,21 @@ exports.getProfile = async (req, res) => {
 exports.addFriend = async (req, res) => {
   try {
     if (req.user.userId === req.params.friendId) {
-      return res.status(400).json({ error: "You cannot add yourself as a friend" });
+      return res
+        .status(400)
+        .json({ error: "You cannot add yourself as a friend" });
     }
 
     const user = await User.findById(req.user.userId);
-    
+
     // Check if already friends
     if (user.friends.includes(req.params.friendId)) {
       return res.status(400).json({ error: "Already in friends list" });
     }
-    
+
     user.friends.push(req.params.friendId);
     await user.save();
-    res.json({ message: 'Friend added successfully' });
+    res.json({ message: "Friend added successfully" });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
@@ -34,8 +36,10 @@ exports.addFriend = async (req, res) => {
 
 exports.getFriends = async (req, res) => {
   try {
-    const user = await User.findById(req.user.userId)
-      .populate('friends', 'username profilePicture');
+    const user = await User.findById(req.user.userId).populate(
+      "friends",
+      "username profilePicture"
+    );
     res.json(user.friends);
   } catch (error) {
     res.status(400).json({ error: error.message });
@@ -45,16 +49,15 @@ exports.getFriends = async (req, res) => {
 exports.getAvailableUsers = async (req, res) => {
   try {
     const currentUser = await User.findById(req.user.userId);
-    
+
     // Get all users except current user and existing friends
     const availableUsers = await User.find({
-      _id: { 
+      _id: {
         $ne: req.user.userId, // not equal to current user
-        $nin: currentUser.friends // not in friends array
-      }
-    })
-    .select('username profilePicture bio');
-    
+        $nin: currentUser.friends, // not in friends array
+      },
+    }).select("username profilePicture bio");
+
     res.json(availableUsers);
   } catch (error) {
     res.status(400).json({ error: error.message });
@@ -64,13 +67,13 @@ exports.getAvailableUsers = async (req, res) => {
 exports.getMyProfile = async (req, res) => {
   try {
     const user = await User.findById(req.user.userId)
-      .select('-password')
-      .populate('friends', 'username profilePicture bio');
-    
+      .select("-password")
+      .populate("friends", "username profilePicture bio");
+
     if (!user) {
       return res.status(404).json({ error: "User not found" });
     }
-    
+
     res.json(user);
   } catch (error) {
     res.status(400).json({ error: error.message });
